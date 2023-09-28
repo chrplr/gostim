987580b46ac703c4dbde91b441ad832a29a51d87
Christophe Pallier <christophe@pallier.org>
2023-09-28 12:24:32 +0200

    added sounded to checks

--- tearing_test/check_tearing.go
+++ tearing_test/check_tearing.go
@@ -5,9 +5,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+
+	//	"math"
+	"os"
+	"runtime"
 	"slices"
 
+	"github.com/jaypipes/ghw"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/veandco/go-sdl2/mix"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -20,7 +29,163 @@ func toMilliseconds(ticks uint64) float64 {
 	return 1000.0 * float64(ticks) / timerResolution
 }
 
+type SysInfo struct {
+	HostName string
+	OS       string
+	CPU      string
+	GPU      []string
+}
+
+func GetSystemInfo() SysInfo {
+	info := SysInfo{
+		HostName: "",
+		OS:       "",
+		CPU:      "",
+		GPU:      []string{},
+	}
+
+	var err error
+
+	info.HostName, err = os.Hostname()
+	if err != nil {
+		log.Printf("Error getting hostname: %v", err)
+	}
+
+	info.OS = runtime.GOOS
+
+	cpu, err := cpu.Info()
+	if err != nil {
+		log.Printf("Error getting CPU info: %v", err)
+	}
+
+	info.CPU = cpu[0].ModelName
+
+	gpu, err := ghw.GPU()
+	if err != nil {
+		log.Printf("Error getting GPU info: %v", err)
+	}
+
+	info.GPU = make([]string, len(gpu.GraphicsCards))
+	for _, card := range gpu.GraphicsCards {
+		info.GPU = append(info.GPU, fmt.Sprintf(" %v\n", card))
+	}
+
+	return info
+}
+
+type SdlInfo struct {
+	Platform       string
+	RAM            int
+	Version        sdl.Version
+	Revision       string
+	AudioDriver    string
+	VideoDriver    string
+	NDisplays      int
+	Displays       string
+	GLSwapInterval int
+	VBlankEnvVar   string
+	GLVBLank       string
+}
+
+func GetSDLInfo() SdlInfo {
+	info := SdlInfo{
+		Platform:       "",
+		RAM:            0,
+		Version:        sdl.Version{},
+		Revision:       "",
+		AudioDriver:    "",
+		VideoDriver:    "",
+		NDisplays:      0,
+		Displays:       "",
+		GLSwapInterval: 0,
+		VBlankEnvVar:   "",
+		GLVBLank:       "",
+	}
+
+	var err error
+
+	info.Platform = sdl.GetPlatform()
+	info.Revision = sdl.GetRevision()
+	sdl.GetVersion(&info.Version)
+	info.RAM = sdl.GetSystemRAM()
+
+	info.AudioDriver = sdl.GetCurrentAudioDriver()
+
+	if info.VideoDriver, err = sdl.GetCurrentVideoDriver(); err != nil {
+		panic(err)
+	}
+
+	// EnvVarsOfInterest = []string{"NvOptimusEnablement", "__NV_PRIME_RENDER_OFFLOAD", "SDL_VIDEODRIVER", "SDL_AUDIODRIVER", "vblank_mode", "__GL_SYNC_TO_VBLANK"}
+
+	/* XDG_SESSION_TYPE ; XDG_SESSION_DESKTOP; DESKTOP_SESSION ; DRI_PRIME=1 ; __NV_PRIME_RENDER_OFFLOAD=1 ; __GLX_VENDOR_LIBRARY_NAME=nvidia */
+	/* NvOptimusEnablement: optimus prime on intel/nvidia hybrid systems */
+	/* SDL_VIDEODRIVER, SDL_AUDIODRIVER (see https://wiki.libsdl.org/SDL2/FAQUsingSDL) */
+	/* vblank_sync (See https://www.opengl.org/wiki/Swap_Interval)
+
+	   0. Never synchronize with vertical refresh, ignore application's choice
+	   1. Initial swap interval 0, obey application's choice
+	   2. Initial swap interval 1, obey application's choice
+	   3. Always synchronize with vertical refresh application chooses the minimum swap interval
+	   4. Adaptative vsync
+
+	*/
+
+	info.VBlankEnvVar = os.Getenv("vblank_mode")
+
+	/*  GL SYNC TO VBLANK  (https://download.nvidia.com/XFree86/Linux-x86_64/304.137/README/openglenvvariables.html)  NVidia specific?
+
+	The __GL_SYNC_TO_VBLANK (boolean) environment variable can be used to control whether swaps are synchronized to a display device's vertical refresh:
+
+	0. allows glXSwapBuffers to swap without waiting for vblank.
+	1. forces glXSwapBuffers to synchronize with the vertical blanking period. This is the default behavior.
+	*/
+
+	info.GLVBLank = os.Getenv("__GL_SYNC_TO_VBLANK")
+
+	info.NDisplays, _ = sdl.GetNumVideoDisplays()
+
+	// func GetDisplayDPI(displayIndex int) (ddpi, hdpi, vdpi float32, err error)
+
+	//DisplayModes := make([]int, info.NDisplays)
+	for displayIndex := 0; displayIndex < info.NDisplays; displayIndex++ {
+		name, _ := sdl.GetDisplayName(displayIndex)
+		info.Displays += fmt.Sprintf("Display:%s\n", name)
+		mode, _ := sdl.GetCurrentDisplayMode(displayIndex)
+		info.Displays += fmt.Sprintf("   Resolution: %dx%d\n", mode.W, mode.H)
+		info.Displays += fmt.Sprintf("   Refresh: %dHz\n", mode.RefreshRate)
+		info.Displays += fmt.Sprintf("   Format: %d (see https://wiki.libsdl.org/SDL_PixelFormatEnum)\n", mode.Format)
+	}
+
+	info.GLSwapInterval, _ = sdl.GLGetSwapInterval()
+	return info
+}
+
+func Abs(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// freq & samplingFreq in Hz, duration in ms
+/* func SineWave(freq int, duration int, amplitude int, samplingFreq int) []int16 {
+	length := (duration * samplingFreq) / 1000
+	signal := make([]int16, length)
+	amplitudef := float64(amplitude)
+	for i := 0; i < length; i++ {
+		signal[i] = int16(amplitudef * math.Sin(2.0*math.Pi*float64(i)/float64(freq)))
+	}
+	return signal
+}
+*/
 func run() (timings []float64) {
+	soundOn := true
+	testSound, err := mix.LoadWAV("tone440.wav")
+	if err != nil {
+		log.Println(err)
+		soundOn = false
+	}
+	defer testSound.Free()
 
 	rectTop := sdl.Rect{X: 0, Y: 0, W: screenWidth, H: 200}
 	rectBottom := sdl.Rect{X: 0, Y: screenHeight - 200, W: screenWidth, H: 200}
@@ -57,21 +222,42 @@ func run() (timings []float64) {
 
 		// display vertical bar & move it
 		bar.X = xpos
-		bar.W = skip
+		bar.W = 8
 		renderer.SetDrawColor(255, 255, 255, 255)
 		renderer.FillRect(&bar)
-		xpos = (xpos + skip) % screenWidth
-
-		// detect any key press
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				running = false
-			case *sdl.KeyboardEvent:
-				running = false
+		if (xpos+skip > 0) && (xpos+skip < screenWidth) {
+			xpos = xpos + skip
+		} else { // invert direction of movement & play tone
+			skip = -skip
+			if soundOn {
+				if _, err := testSound.Play(-1, 0); err != nil {
+					log.Println(err)
+				}
 			}
 		}
 
+		// Process key press
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			case *sdl.KeyboardEvent:
+				switch t.Keysym.Sym {
+				case sdl.K_ESCAPE:
+					running = false
+				case sdl.K_RIGHT:
+					skip = skip + 8
+					log.Printf("skip = %d W=%d\n", skip, bar.W)
+				case sdl.K_LEFT:
+					if Abs(skip) >= 16 {
+						skip = skip - 8
+						log.Printf("skip = %d W=%d\n", skip, bar.W)
+					}
+				}
+			}
+		}
+
+		sdl.Delay(3)
 		renderer.Present()
 
 		delta := toMilliseconds(sdl.GetPerformanceCounter() - start)
@@ -93,21 +279,58 @@ func printHistogram(timings []float64) {
 	}
 }
 
+/* func isMouseMotionEvent(event sdl.Event, userdata interface{}) bool {
+	return event.GetType() == sdl.MOUSEMOTION
+}
+*/
+
 func main() {
+	DesktopMode := flag.Bool("desktop", true, "true: FULLSCREEN_DESKTOP; false: FULLSCREEN")
 	var err error
 
-	//window, err := sdl.CreateWindow("", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-	//	0, 0, sdl.WINDOW_FULLSCREEN_DESKTOP)
-	if window, err = sdl.CreateWindow("", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 1920, 1080, sdl.WINDOW_FULLSCREEN); err != nil {
-		panic(err)
+	flag.Parse()
+	fmt.Printf("%+V\n", DesktopMode)
+
+	fmt.Printf("System = %+v\n", GetSystemInfo())
+
+	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		log.Fatalln(err)
+	}
+	defer sdl.Quit()
+
+	if *DesktopMode {
+		if window, err = sdl.CreateWindow("", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 0, 0, sdl.WINDOW_FULLSCREEN_DESKTOP); err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		if window, err = sdl.CreateWindow("", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 1920, 1080, sdl.WINDOW_FULLSCREEN); err != nil {
+			log.Fatalln(err)
+		}
 	}
 	defer window.Destroy()
 
 	if renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC); err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	defer renderer.Destroy()
 
+	// Initialize mixer
+	if err = mix.Init(mix.INIT_MP3); err != nil {
+		log.Fatalln(err)
+	}
+	defer mix.Quit()
+
+	// Open default playback device
+	if err := mix.OpenAudio(mix.DEFAULT_FREQUENCY, mix.DEFAULT_FORMAT, mix.DEFAULT_CHANNELS, mix.DEFAULT_CHUNKSIZE); err != nil {
+		log.Fatalln(err)
+	}
+	defer mix.CloseAudio()
+
+	sdl.DisableScreenSaver()
+	sdl.ShowCursor(0)
+	// sdl.SetEventFilter(isMouseMotionEvent, nil)
+
+	fmt.Printf("SDL2 = %+v\n", GetSDLInfo())
 	sdl.Delay(3000)
 
 	screenWidth, screenHeight, _ = renderer.GetOutputSize()
--- tearing_test/go.mod
+++ tearing_test/go.mod
@@ -2,4 +2,25 @@ module example/tearing
 
 go 1.18
 
-require github.com/veandco/go-sdl2 v0.4.21
+require (
+	github.com/jaypipes/ghw v0.12.0
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	github.com/veandco/go-sdl2 v0.4.21
+)
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jaypipes/pcidb v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
